1e8ec0ba5d5b12c0ba748df5cddedc691138b372
AnvitaMurthy <56925491+AnvitaMurthy@users.noreply.github.com>
2020-09-06 23:21:54 +0530

    Updates

---
+++ precache-manifest.2b110449e4d4df1deffdda7a84d7b587.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb297c49f17b43a428e81c401fa1858",
+    "url": "/hello-manny/index.html"
+  },
+  {
+    "revision": "fca526acc6277759d7b0",
+    "url": "/hello-manny/static/css/2.8f118048.chunk.css"
+  },
+  {
+    "revision": "1e0d36f2ec626d77c011",
+    "url": "/hello-manny/static/css/main.80cbfdc8.chunk.css"
+  },
+  {
+    "revision": "fca526acc6277759d7b0",
+    "url": "/hello-manny/static/js/2.5d48d36f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-manny/static/js/2.5d48d36f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0d36f2ec626d77c011",
+    "url": "/hello-manny/static/js/main.62117006.chunk.js"
+  },
+  {
+    "revision": "276e5bf19623113a2811",
+    "url": "/hello-manny/static/js/runtime-main.5be22c46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-manny/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "79940b844411e1b6a6777417c9c0bba0",
+    "url": "/hello-manny/static/media/m1.79940b84.mp4"
+  },
+  {
+    "revision": "ebcded71643842563f2e98194de1f28a",
+    "url": "/hello-manny/static/media/m2.ebcded71.mp4"
+  },
+  {
+    "revision": "3693ff3b58e36cb8f42dc169559eea5d",
+    "url": "/hello-manny/static/media/m3.3693ff3b.mp4"
+  },
+  {
+    "revision": "0f973208d69209237f5439ef8c57bb98",
+    "url": "/hello-manny/static/media/m4.0f973208.mp4"
+  },
+  {
+    "revision": "8a852b03d82b1a8875ed21a198e26480",
+    "url": "/hello-manny/static/media/m5.8a852b03.mp4"
+  },
+  {
+    "revision": "fa6f4d74a8f021b5ff5c771d8e702280",
+    "url": "/hello-manny/static/media/m7.fa6f4d74.mp4"
+  }
+]);
